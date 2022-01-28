244b4cbd4429acf5b25d5cbef34736748f7b65f6
saifalishaikh31 <saifalishaikh31>
2022-01-28 08:25:55 +0530

    [Saif Ali] Add: Length of Two Points Using Cartesian System program Added

---
+++ LineComparison/CalculateDistance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LineComparison
+{
+    class CalculateDistance
+    {
+        public void DistanceResult()
+        {
+           
+
+            int x1, y1, x2, y2;
+            double result;
+            Console.WriteLine("Enter the Co-ordinates of first point as A(x1,y1) : ");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Co-ordinates of second point as B(x2,y2) : ");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("The Length of 2 points is : "+result);    
+            
+        }
+
+        
+        }
+    }
+
--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -10,6 +10,8 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
+            CalculateDistance calulatedistance = new CalculateDistance();
+            calulatedistance.DistanceResult();
             Console.ReadLine();
         }
     }
